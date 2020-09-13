b111879c5f02e5ea831ebba34654c12f3ccb99ec
Official21A <officialamirhossein21@gmail.com>
2020-09-13 09:48:58 +0430

    start added

--- Console.py
+++ Console.py
@@ -9,7 +9,30 @@ def get_line_break():
     return string
 
 
+def input_massage():
+    string = "> Enter your commands like : $ <command>"
+    string += "\n" + "  -- new  => for executing the committer builder."
+    string += "\n" + "  -- quit => for exiting the program"
+
+
 def init():
     print(">>> Committer started ...")
     program_setups_check()
     print(get_line_break())
+
+
+def start_console():
+    init()
+    input_massage()
+    while True:
+        order = input("$ ")
+        order = order.strip()
+        if order == "quit":
+            break
+        elif order == "new":
+            execute()
+            push_message()
+        else:
+            print("> Not valid.")
+        print(get_line_break())
+
