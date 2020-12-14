8901446fa7fa13db2824370ddab2f73d0ad61ce3
Official21A <officialamirhossein21@gmail.com>
2020-12-14 08:38:30 +0330

    add set class

--- dir_check.py
+++ dir_check.py
@@ -2,7 +2,15 @@ import os
 
 
 # This function checks if the directory that we work on is exist
-def dir_init():
-    path = "./Documents"
+def dir_init(path="./Documents"):
     if not os.path.exists(path):
         os.mkdir(path)
+
+
+# This function checks the directory existence inside the documents
+def in_doc_search(path="Empty"):
+    if path == "Empty":
+        return False
+    else:
+        dir_init("./Documents/"+path)
+        return True
