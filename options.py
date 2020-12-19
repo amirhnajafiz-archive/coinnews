cfbbe63540efd04ce6540a060ad7ce7a993dce2a
Official21A <officialamirhossein21@gmail.com>
2020-12-19 07:45:24 +0330

    options class

---
+++ options.py
@@ -0,0 +1,13 @@
+class Option:
+    def __init__(self):
+        self.files = []
+
+    def initialize(self):
+        try:
+            with open("tools.txt", "r") as file:
+                self.files = [line for line in file.readlines()]
+        except FileNotFoundError as error:
+            print(error)
+
+    def get_file(self, index):
+        return self.files[index]
