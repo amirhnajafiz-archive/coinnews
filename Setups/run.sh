22831c4ef70928bacccfd1ed34bf5ce2ad77cde5
Official21A <officialamirhossein21@gmail.com>
2020-12-28 22:20:42 +0330

    edit setups

--- Setups/run.sh
+++ Setups/run.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/bash
 
-# Getting the date from user
-echo "Enter your date : "
-date_time=$(date)
-read -e date_time
+# Checking in the branch user wants
+git checkout $1
 
 # Performing a commit to the date of the user
 git add .
-git commit --amend --no-edit --date="$date_time"
+git commit --amend --no-edit --date="$2"
 
 xdotool key "control+x"
-git pull --allow-unrelated-histories "https://github.com/Official21A/CommiterTestBench.git"
+git pull --allow-unrelated-histories "$3"
 # Put this in instaltion tool kit
 # apt install xdotool
 
-git push -u origin master
+git push -u origin $1
