033ea5358e6646c1561bfe07b2eaf19e486e7ac4
Official21A <officialamirhossein21@gmail.com>
2020-12-29 08:24:56 +0330

    user date input add

---
+++ Committer.py
@@ -0,0 +1,22 @@
+# This is our program main file which will connect the application part together
+from Tools.Builder import execute
+from Configure import push_to_repo, setup_git, is_config
+
+
+# This function will setup the basics if needed
+def program_setups_check():
+    if not is_config():
+        url = input("Please enter the git url >> ")
+        branch = input("Please enter the name of the branch >> ")
+        setup_git(url, branch)
+
+
+# This function will get the commit date from user
+def get_date_from_user():
+    return input("Please enter your date (or type nil for today date) >> ")
+
+
+if __name__ == "__main__":
+    program_setups_check()
+    execute()
+    push_to_repo(commit_date=get_date_from_user())
