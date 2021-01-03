a369f710791745ac8894ab4a93b3b8a29ffbd3fe
Official21A <officialamirhossein21@gmail.com>
2021-01-03 08:49:34 +0330

    Add cache in

--- Cache/Cacher.py
+++ Cache/Cacher.py
@@ -15,5 +15,13 @@ def cache_up(string):
     with open(local_path, "r") as file:
         lines = file.readlines()
     with open(local_path, "w") as file:
-        new_lines = [line for line in lines if line != "$"+string]
+        new_lines = [line for line in lines if line != "&"+string]
         file.writelines(new_lines)
+        file.write(f'\n&{string}')
+
+
+def cache_in():
+    with open(local_path, "r") as file:
+        lines = file.readlines()
+        new_lines = [line for line in lines if line.startswith("&")]
+    return new_lines
