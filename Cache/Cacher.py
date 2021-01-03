2c32713a3b09cf18968dba51e2857481a76c10b0
Official21A <officialamirhossein21@gmail.com>
2021-01-03 08:45:58 +0330

    Cache up

--- Cache/Cacher.py
+++ Cache/Cacher.py
@@ -2,7 +2,18 @@ import os
 import datetime
 
 
+local_path = "/Cache/record.data"
+
+
 def cache_init():
-    if not os.path.exists("/Cache/record.data"):
-        with open("/Cache/record.data", "w") as file:
+    if not os.path.exists(local_path):
+        with open(local_path, "w") as file:
             file.writelines([f'$Cache Created : {datetime.datetime.now()}'])
+
+
+def cache_up(string):
+    with open(local_path, "r") as file:
+        lines = file.readlines()
+    with open(local_path, "w") as file:
+        new_lines = [line for line in lines if line != "$"+string]
+        file.writelines(new_lines)
