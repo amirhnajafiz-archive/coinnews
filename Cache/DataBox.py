0a96efa4676f5983678ca083d735ef90e0238421
Official21A <officialamirhossein21@gmail.com>
2021-01-04 08:25:42 +0330

    Add data box

---
+++ Cache/DataBox.py
@@ -0,0 +1,10 @@
+class Box:
+    def __init__(self):
+        self.file_number = 0
+        self.commit_date = ""
+        self.files_list = []
+
+    def get_string(self):
+        string = f'$${self.file_number}'
+        string += f'$${self.commit_date}'
+        string += f'$${self.files_list}'
