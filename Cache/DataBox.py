4bb1e6c0f6a499ca9ccbcb0c5cf92ea19ce908ca
Official21A <officialamirhossein21@gmail.com>
2021-01-04 09:13:32 +0330

    Save to cache added

--- Cache/DataBox.py
+++ Cache/DataBox.py
@@ -4,9 +4,3 @@ class Box:
         self.commit_date = ""
         self.files_list = []
 
-    @property
-    def get_string(self):
-        string = f'$${self.file_number}'
-        string += f'$${self.commit_date}'
-        string += f'$${self.files_list}'
-        return string
