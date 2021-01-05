71170cd2f2280e497ab6627ba65d57e3f96e120e
Official21A <officialamirhossein21@gmail.com>
2021-01-05 09:06:33 +0330

    Cache in update

--- Cache/Cacher.py
+++ Cache/Cacher.py
@@ -8,7 +8,7 @@ local_path = "record.data"
 def cache_init():
     if not os.path.exists(local_path):
         with open(local_path, "w") as file:
-            file.writelines([f'$Cache Created : {datetime.datetime.now()}'])
+            file.writelines([f'$._Cache Created : {datetime.datetime.now()}'])
 
 
 def cache_up(box):
@@ -16,16 +16,26 @@ def cache_up(box):
         lines = file.readlines()
     with open(local_path, "w") as file:
         file.writelines(lines)
-        file.write(f'\n&$${box.file_number}$${box.files_list}')
+        file.write(f'\n&._${box.file_number}_${box.files_list}_$#')
 
 
 def cache_in():
+    cache_lines = {}
     with open(local_path, "r") as file:
         lines = file.readlines()
-        new_lines = [line for line in lines if line.startswith("&")]
-    return new_lines
+        index = 0
+        for line in lines:
+            if line.startswith("$"):
+                continue
+            parts = line.strip().split("_$")
+            total_number = int(parts[1])
+            files = parts[2].split(" ")
+            files = [int(number) for number in files]
+            cache_lines[str(index)] = (total_number, files)
+            index += 1
+    return cache_lines
 
 
 def cache_clear():
     with open(local_path, "w") as file:
-        file.writelines([f'$Cache Created : {datetime.datetime.now()}'])
+        file.writelines([f'$._Cache Created : {datetime.datetime.now()}'])
