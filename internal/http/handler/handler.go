b283a0b7dfce334f55d7a61eb5c76be568e0b7d9
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-23 10:06:34 +0330

    init: get data

--- internal/http/handler/handler.go
+++ internal/http/handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"strconv"
 )
 
 type Handler struct {
@@ -9,5 +10,11 @@ type Handler struct {
 }
 
 func (h *Handler) GetData(ctx *fiber.Ctx) error {
-	return ctx.SendStatus(fiber.StatusNotImplemented)
+	name := ctx.Params("name", "bitcoin")
+
+	if value, ok := h.crypto[name]; ok {
+		return ctx.Status(fiber.StatusOK).SendString(strconv.Itoa(value))
+	}
+
+	return ctx.SendStatus(fiber.StatusNotFound)
 }
