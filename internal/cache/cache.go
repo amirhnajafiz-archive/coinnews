9fe658d2032382152cfa9da1f4bf813bcdb5a509
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-24 11:40:43 +0330

    update: init cache

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -4,14 +4,14 @@ type Cache struct {
 	crypto map[string]int
 }
 
-func New() *Cache {
-	return &Cache{
-		crypto: make(map[string]int),
-	}
-}
+func New(values ...string) *Cache {
+	list := make(map[string]int)
 
-func (c *Cache) InitValues(values ...string) {
 	for _, item := range values {
-		c.crypto[item] = 0
+		list[item] = 0
+	}
+
+	return &Cache{
+		crypto: list,
 	}
 }
