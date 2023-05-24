e2fdfbd009be5a93f5d4e770d45240bd715bd488
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-24 11:42:34 +0330

    add: get method for cache

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,9 +1,18 @@
 package cache
 
+import (
+	"errors"
+)
+
+var (
+	errNotFound = errors.New("item not found")
+)
+
 type Cache struct {
 	crypto map[string]int
 }
 
+// New returns a new cache module.
 func New(values ...string) *Cache {
 	list := make(map[string]int)
 
@@ -14,4 +23,13 @@ func New(values ...string) *Cache {
 	return &Cache{
 		crypto: list,
 	}
+}
+
+// Get an item by it's name.
+func (c *Cache) Get(name string) (int, error) {
+	if value, ok := c.crypto[name]; ok {
+		return value, nil
+	}
+
+	return 0, errNotFound
 }
