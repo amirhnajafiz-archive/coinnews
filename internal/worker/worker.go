da68e5158235c0a37c7fefa77cfc7e99c5ae8101
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-26 13:19:57 +0330

    feat: worker

--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -1,8 +1,10 @@
 package worker
 
 import (
-	"data-generator/internal/cache"
+	"math/rand"
 	"time"
+
+	"data-generator/internal/cache"
 )
 
 type Worker struct {
@@ -11,7 +13,26 @@ type Worker struct {
 }
 
 func (w *Worker) Do() {
+	names := w.Cache.GetAllNames()
+
 	for {
+		for _, name := range names {
+			w.process(name)
+		}
+
 		time.Sleep(time.Duration(w.Interval) * time.Millisecond)
 	}
+}
+
+func (w *Worker) process(name string) {
+	value, _ := w.Cache.Get(name)
+
+	delta := rand.Int() % 5
+	if delta == 0 || delta == 3 {
+		value -= 1
+	} else if delta == 1 || delta == 2 {
+		value += 1
+	}
+
+	w.Cache.Update(name, delta)
 }
