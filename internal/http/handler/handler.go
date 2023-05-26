b778bd340cf529cfea80e89fa8b258eb6006e668
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-26 16:16:39 +0330

    fix: http handler

--- internal/http/handler/handler.go
+++ internal/http/handler/handler.go
@@ -1,23 +1,24 @@
 package handler
 
 import (
+	"data-generator/internal/cache"
 	"data-generator/internal/http/response"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 type Handler struct {
-	crypto map[string]int
+	Cache *cache.Cache
 }
 
 func (h *Handler) GetData(ctx *fiber.Ctx) error {
 	name := ctx.Params("name", "bitcoin")
 
-	if value, ok := h.crypto[name]; ok {
+	if value, err := h.Cache.Get(name); err == nil {
 		return ctx.Status(fiber.StatusOK).JSON(response.CryptoResponse{
-			Type:  "USD",
-			Name:  name,
-			Value: value,
+			Type:  value.Type,
+			Name:  value.Name,
+			Value: value.Value,
 		})
 	}
 
