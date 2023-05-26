bc5fe07d7e5d5cb19fe80cf0fb59371058b9b974
amirhnajafiz <najafizadeh21@gmail.com>
2023-05-26 11:42:11 +0330

    init: worker

--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -1,4 +1,17 @@
 package worker
 
+import (
+	"data-generator/internal/cache"
+	"time"
+)
+
 type Worker struct {
+	Cache    *cache.Cache
+	Interval int
+}
+
+func (w *Worker) Do() {
+	for {
+		time.Sleep(time.Duration(w.Interval) * time.Millisecond)
+	}
 }
