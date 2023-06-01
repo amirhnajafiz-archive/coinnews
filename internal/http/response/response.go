0273e35c9f68fb0c7572b01321fb9fd2c6b259de
amirhnajafiz <najafizadeh21@gmail.com>
2023-06-01 09:39:41 +0330

    update: response

--- internal/http/response/response.go
+++ internal/http/response/response.go
@@ -1,6 +1,11 @@
 package response
 
+import "time"
+
 type CryptoResponse struct {
-	Name  string `json:"name"`
-	Value int    `json:"value"`
+	Name        string    `json:"name"`
+	Value       int       `json:"value"`
+	MarketValue int       `json:"market_value"`
+	ROC         float64   `json:"roc"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
