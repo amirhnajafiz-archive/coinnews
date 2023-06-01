7d76e04e9bfe0a06753c1dd635329bcb6ab3234f
amirhnajafiz <najafizadeh21@gmail.com>
2023-06-01 09:42:46 +0330

    add: get history method

--- internal/http/handler/handler.go
+++ internal/http/handler/handler.go
@@ -3,7 +3,6 @@ package handler
 import (
 	"data-generator/internal/cache"
 	"data-generator/internal/http/response"
-	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -19,8 +18,6 @@ func (h *Handler) GetAvailable(ctx *fiber.Ctx) error {
 func (h *Handler) GetData(ctx *fiber.Ctx) error {
 	name := ctx.Params("name", "bitcoin")
 
-	log.Printf("request for: %s\n", name)
-
 	if value, err := h.Cache.GetInfo(name); err == nil {
 		return ctx.Status(fiber.StatusOK).JSON(response.CryptoResponse{
 			Name:        name,
@@ -29,6 +26,16 @@ func (h *Handler) GetData(ctx *fiber.Ctx) error {
 			ROC:         value.ROC,
 			UpdatedAt:   value.UpdatedAt,
 		})
+	}
+
+	return ctx.SendStatus(fiber.StatusNotFound)
+}
+
+func (h *Handler) GetHistory(ctx *fiber.Ctx) error {
+	name := ctx.Params("name", "bitcoin")
+
+	if value, err := h.Cache.GetInfo(name); err == nil {
+		return ctx.Status(fiber.StatusOK).JSON(value.Changes)
 	}
 
 	return ctx.SendStatus(fiber.StatusNotFound)
